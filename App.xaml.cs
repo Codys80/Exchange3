40f581c4be7774e82461e9c58aaae531dfd7588d
CodyS80 <bartolosemolo@gmail.com>
2024-10-28 11:06:45 +0100

    Dodano projekt

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Exchange3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Exchange3
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
